d1d7bd967345939b3086826d24ce3a94690b0381
Iago Diniz Martins <iagodinizmartins@gmail.com>
2021-08-18 15:15:27 -0300

    Matrizes

---
+++ ConsoleApp1/Matrizes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1 
+{
+    class Matrizes{ 
+        static void Main(string[] args) 
+        {
+            int[,] array = new int[2,2]{{1,2},{3,4}};
+
+            foreach( int colum in array)
+            {
+                Console.WriteLine(colum);
+            }
+        }
+    }
+}
