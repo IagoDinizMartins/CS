07ea2a1d82df8e73477442b4ae3906f527371bb3
Iago Diniz Martins <iagodinizmartins@gmail.com>
2021-08-18 16:41:40 -0300

    Criando e manipulando listas.

---
+++ ConsoleApp1/Lista.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1{ 
+    class Lista {
+        static void Main(string[] args) {
+            List<int> lista = new List<int>();
+            
+            // adding datas to list
+            for ( int i = 0; i < 10; i++)
+            {
+                lista.Add(i * i);
+                Console.WriteLine(lista[i]);
+            }
+
+            Console.WriteLine("");
+
+            // removing datas from list
+            for(int i = 0; i< lista.Count; i++) {
+                if (lista[i] % 2 != 0) {
+                    lista.Remove(lista[i]);
+                    i -= 1;
+                    continue;
+                }
+                Console.WriteLine(lista[i]);
+            }
+        }
+    }
+}
