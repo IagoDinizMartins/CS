53d7b83b443d066eb1fc99ba1ba312be417773c4
Iago Diniz Martins <iagodinizmartins@gmail.com>
2021-08-18 21:18:56 -0300

    Criando e manipulando dicionário.

---
+++ ConsoleApp1/Dicionario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ConsoleApp1{
+
+    class Dicionario
+    {
+        static void Main(string[] args) 
+        {
+            Dictionary<string, string> casais = new Dictionary<string, string>();
+            
+            casais.Add("Verônica", "Estáquio");
+            casais.Add("Susana", "Teseu");
+            casais.Add("Felicidade", "Arthur");
+
+            foreach (string keys in casais.Keys) {
+                Console.WriteLine(casais[keys]);
+            }
+        }
+
+    }
+}
