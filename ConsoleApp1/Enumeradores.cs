9b06df53204252a560433ff653ff79a600e0febf
Iago Diniz Martins <iagodinizmartins@gmail.com>
2021-08-19 08:36:08 -0300

    Criando e manipulando enumeradores.

---
+++ ConsoleApp1/Enumeradores.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    public class Enumeradores 
+    {
+        enum Atos { Rezar = 1, Correr, Lutar}
+        
+        static void Main(string[] args)
+        {
+            Atos ato1 = Atos.Rezar;
+            Atos ato2 = Atos.Lutar;
+
+            Console.WriteLine(ato1);
+            Console.WriteLine(ato2);
+            Console.WriteLine((int)ato1);
+            Console.WriteLine((int)ato2);
+        }
+    }
+}
